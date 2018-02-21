61a4d811437079398509b1bbee5aa58ecd3ae395
Fernando Terra <nandobrt@gmail.com>
2018-02-21 17:01:44 -0300

    test: Add gift

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
 const app = shallow(<App />);
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   expect(app).toMatchSnapshot();
 });
 
-it('initializes the `state` with an empty list of gifts', () => {
+it("initializes the `state` with an empty list of gifts", () => {
   expect(app.state().gifts).toEqual([]);
+});
+
+it("adds a new gift to `state` when clicking `add gift` button", () => {
+  app.find(".btn-add").simulate("click");
+  expect(app.state().gifts).toEqual([
+    {
+      id: 1
+    }
+  ]);
 });
