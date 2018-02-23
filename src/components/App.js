41d81907a19924865cfea054c7f9255e13210c12
Fernando Terra <nandobrt@gmail.com>
2018-02-23 17:27:38 -0300

    style: Format App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
