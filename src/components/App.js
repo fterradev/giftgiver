5915f9cc8a98eb2dcdb37b46fe20727c0722da0c
Fernando Terra <nandobrt@gmail.com>
2018-02-26 15:39:33 -0300

    refactor: Extract max_number function to helper module

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper';
 
 class App extends Component {
   constructor() {
@@ -13,9 +14,8 @@ class App extends Component {
   addGift = () => {
     const { gifts } = this.state;
     const ids = gifts.map(gift => gift.id);
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
     this.setState({
-      gifts: gifts.concat({ id: max_id + 1 })
+      gifts: gifts.concat({ id: max_number(ids) + 1 })
     });
   };
 
